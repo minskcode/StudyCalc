59c746d61057fe96f05e8ab58d6f0c0cca6cfe72
Nik Degilevich <ndegilevich@gmail.com>
2022-12-06 15:59:34 +0300

    06.12.2022 calc

--- StudyCalc/main.swift
+++ StudyCalc/main.swift
@@ -2,6 +2,7 @@
 //  StudyCalc
 // created by Nik Degilevich on 04.12.2022
 
+/*
 print("Добро пожаловать в программу калькулятор")
 print("Выберите операцию: +, -, * или /")
 let operation = readLine() ?? ""
@@ -34,4 +35,36 @@ else if operation == "/" {
 }
 else {
     print("Error")
+}
+*/
+
+print("Добро пожаловать в программу калькулятор")
+
+print("Выберите операцию: +, -, *, /")
+let operation = readLine() ?? ""
+print("Введите первое целое число:")
+let firstNumber = readLine() ?? ""
+print("Введите второе целое число:")
+let secondNumber = readLine() ?? ""
+
+print("Идет вычисление примера: " + firstNumber + " " + operation + " " + secondNumber)
+if let firstNumber = Int(firstNumber) {
+    if let secondNumber = Int(secondNumber) {
+        switch operation {
+        case "+": print("Результат: " + String(firstNumber + secondNumber))
+        case "-": print("Результат: " + String(firstNumber - secondNumber))
+        case "*": print("Результат: " + String(firstNumber * secondNumber))
+        case "/":
+            if secondNumber != 0 {
+                print("Результат: " + String(firstNumber / secondNumber))
+            } else {
+                print("Делить на 0 нельзя")
+            }
+        default: print("Вы некорректно ввели операцию")
+        }
+    } else {
+        print("Вы ввели некорректное второе число")
+    }
+} else {
+    print("Вы ввели некорректное первое число")
 }
